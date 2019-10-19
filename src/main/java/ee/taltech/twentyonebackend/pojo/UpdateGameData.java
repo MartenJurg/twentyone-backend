999b49a6690c453ce75c71e30ae22cda50c975b3
eghunt <eghunt@ttu.ee>
2019-10-19 23:59:54 +0300

    Placeholder game updater.

---
+++ src/main/java/ee/taltech/twentyonebackend/pojo/UpdateGameData.java
@@ -0,0 +1,20 @@
+package ee.taltech.twentyonebackend.pojo;
+
+public class UpdateGameData {
+
+    public boolean cook(String username, String dish){
+        return true;
+    }
+
+    public boolean craft(String username, String craft) {
+        return true;
+    }
+
+    public boolean beverage(String username, String drink) {
+        return true;
+    }
+
+    public boolean steal(String username, String item) {
+        return true;
+    }
+}
