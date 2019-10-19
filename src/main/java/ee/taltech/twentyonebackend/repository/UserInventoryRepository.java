6d0426588dcc899d1a3e61097b43e5236d5deb57
eghunt <eghunt@ttu.ee>
2019-10-20 00:38:35 +0300

    Made inventory repository and started inventory service.

--- src/main/java/ee/taltech/twentyonebackend/repository/UserInventoryRepository.java
+++ src/main/java/ee/taltech/twentyonebackend/repository/UserInventoryRepository.java
@@ -1,9 +1,11 @@
 package ee.taltech.twentyonebackend.repository;
 
-import ee.taltech.twentyonebackend.model.UserData;
+import ee.taltech.twentyonebackend.model.UserInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
-public interface UserInventoryRepository  extends JpaRepository<UserData, Long> {
+public interface UserInventoryRepository  extends JpaRepository<UserInventory, Long> {
+    Optional<UserInventory> findByUsername(String username);
 }
--- src/main/java/ee/taltech/twentyonebackend/service/UserInventoryService.java
+++ src/main/java/ee/taltech/twentyonebackend/service/UserInventoryService.java
@@ -1,7 +1,23 @@
 package ee.taltech.twentyonebackend.service;
 
+import ee.taltech.twentyonebackend.exception.UserNotFoundException;
+import ee.taltech.twentyonebackend.model.UserInventory;
+import ee.taltech.twentyonebackend.pojo.UserInventoryDto;
+import ee.taltech.twentyonebackend.repository.UserInventoryRepository;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class UserInventoryService {
+
+    @Resource
+    UserInventoryRepository userInventoryRepository;
+
+    public UserInventoryDto getByUsername(String username) throws UserNotFoundException {
+
+        UserInventory userInventory = userInventoryRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+
+        return new UserInventoryDto(userInventory);
+    }
 }
