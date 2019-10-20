daf4e964ef403017353bd6397396d403155f4ca9
eghunt <eghunt@ttu.ee>
2019-10-20 04:32:41 +0300

    Able to sell the items you craft / steal.

--- src/main/java/ee/taltech/twentyonebackend/service/UserDataService.java
+++ src/main/java/ee/taltech/twentyonebackend/service/UserDataService.java
@@ -2,8 +2,10 @@ package ee.taltech.twentyonebackend.service;
 
 import ee.taltech.twentyonebackend.exception.UserNotFoundException;
 import ee.taltech.twentyonebackend.model.UserData;
+import ee.taltech.twentyonebackend.model.UserInventory;
 import ee.taltech.twentyonebackend.pojo.UserDataDto;
 import ee.taltech.twentyonebackend.repository.UserDataRepository;
+import ee.taltech.twentyonebackend.repository.UserInventoryRepository;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
 
@@ -12,6 +14,9 @@ public class UserDataService {
 
     @Resource
     UserDataRepository userDataRepository;
+
+    @Resource
+    UserInventoryRepository userInventoryRepository;
 
     public UserDataDto getByUsername(String username) throws UserNotFoundException {
 
@@ -43,21 +48,20 @@ public class UserDataService {
         }
     }
 
-    public void updateCrafting(String username, String craft) {
+    public void updateCrafting(String username) {
         UserData userData = userDataRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
-        switch (craft){
-            case "gloves":
-                userData.setCash(userData.getCash() + 5);
-                save(userData);
-                break;
-            case "hat":
-                userData.setCash(userData.getCash() + 20);
-                save(userData);
-                break;
-            case "sweater":
-                userData.setCash(userData.getCash() + 50);
-                save(userData);
-                break;
+        UserInventory userInventory = userInventoryRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+        for (int i = 0; i < userInventory.getGloves(); i++) {
+            userData.setCash(userData.getCash() + 5);
+            userInventory.setGloves(userInventory.getGloves() - 1);
+        }
+        for (int i = 0; i < userInventory.getHats(); i++) {
+            userData.setCash(userData.getCash() + 20);
+            userInventory.setHats(userInventory.getHats() - 1);
+        }
+        for (int i = 0; i < userInventory.getSweaters(); i++) {
+            userData.setCash(userData.getCash() + 50);
+            userInventory.setSweaters(userInventory.getSweaters() - 1);
         }
     }
 
@@ -85,21 +89,20 @@ public class UserDataService {
         }
     }
 
-    public void updateThieving(String username, String thieving) {
+    public void updateThieving(String username) {
         UserData userData = userDataRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
-        switch (thieving){
-            case "paper":
-                userData.setCash(userData.getCash() + 5);
-                save(userData);
-                break;
-            case "watch":
-                userData.setCash(userData.getCash() + 20);
-                save(userData);
-                break;
-            case "phone":
-                userData.setCash(userData.getCash() + 50);
-                save(userData);
-                break;
+        UserInventory userInventory = userInventoryRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+        for (int i = 0; i < userInventory.getPaper(); i++) {
+            userData.setCash(userData.getCash() + 5);
+            userInventory.setPaper(userInventory.getPaper() - 1);
+        }
+        for (int i = 0; i < userInventory.getHats(); i++) {
+            userData.setCash(userData.getCash() + 20);
+            userInventory.setWatches(userInventory.getWatches() - 1);
+        }
+        for (int i = 0; i < userInventory.getPhones(); i++) {
+            userData.setCash(userData.getCash() + 50);
+            userInventory.setPhones(userInventory.getPhones() - 1);
         }
     }
 
