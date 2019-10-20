778ba1b26f5692419a155503b3f586273d19fb11
eghunt <eghunt@ttu.ee>
2019-10-20 05:11:08 +0300

    Added a fame update.

--- src/main/java/ee/taltech/twentyonebackend/pojo/UpdateGameData.java
+++ src/main/java/ee/taltech/twentyonebackend/pojo/UpdateGameData.java
@@ -17,25 +17,35 @@ public class UpdateGameData {
 
     public void cook(String username, String dish){
         userDataService.updateCooking(username, dish);
+        userDataService.updateFame(username);
     }
 
     public void craft(String username, String craft) {
         userInventoryService.updateCrafting(username, craft);
+        userDataService.updateFame(username);
     }
 
     public void beverage(String username, String drink) {
         userDataService.updateBeverage(username, drink);
+        userDataService.updateFame(username);
     }
 
     public void steal(String username, String item) {
         userInventoryService.updateThieving(username, item);
+        userDataService.updateFame(username);
     }
 
     public void sellCrafts(String username) {
         userDataService.updateCrafting(username);
+        userDataService.updateFame(username);
     }
 
     public void sellThievingItems(String username) {
         userDataService.updateThieving(username);
+        userDataService.updateFame(username);
+    }
+
+    private void updateFame(String username) {
+        userDataService.updateFame(username);
     }
 }
