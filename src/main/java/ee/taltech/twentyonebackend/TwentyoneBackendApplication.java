f9a71ab9d48e5fd73f6b1b6d0905cdfc1ba567d8
eghunt <eghunt@ttu.ee>
2020-01-04 03:24:14 +0200

    Made a bean for admin.

--- src/main/java/ee/taltech/twentyonebackend/TwentyoneBackendApplication.java
+++ src/main/java/ee/taltech/twentyonebackend/TwentyoneBackendApplication.java
@@ -1,7 +1,13 @@
 package ee.taltech.twentyonebackend;
 
+import ee.taltech.twentyonebackend.pojo.RoleName;
+import ee.taltech.twentyonebackend.security.model.User;
+import ee.taltech.twentyonebackend.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
 
 
 @SpringBootApplication
@@ -12,4 +18,17 @@ public class TwentyoneBackendApplication {
 
 	}
 
+	@Bean
+	public CommandLineRunner initUser(UserService userservice) {
+		return (args) -> {
+			 User user = new User(
+			 		"root",
+					 "admin",
+					 "admin@hotmail.com",
+					 "admin",
+					 RoleName.ROLE_ADMIN
+			 );
+			 userservice.save(user);
+		};
+	}
 }
