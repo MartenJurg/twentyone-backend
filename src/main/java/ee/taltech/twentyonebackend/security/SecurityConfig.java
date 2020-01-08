b00396fae9ce688895b9af1e5675229ccac4ac15
eghunt <eghunt@ttu.ee>
2020-01-08 15:50:23 +0200

    Https strict transport security added to security conf
    Egert Kristopher Hunt

--- src/main/java/ee/taltech/twentyonebackend/security/SecurityConfig.java
+++ src/main/java/ee/taltech/twentyonebackend/security/SecurityConfig.java
@@ -41,6 +41,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
+                .headers().httpStrictTransportSecurity().disable()
+                .and()
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .exceptionHandling().authenticationEntryPoint(restAuthenticationEntryPoint)
