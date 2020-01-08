cf802a8c4d74725d846bc771ff6ed8b3fa8ad47f
eghunt <eghunt@ttu.ee>
2020-01-08 13:15:37 +0200

    Added getters setters to user response data
    Egert Kristopher Hunt

--- src/main/java/ee/taltech/twentyonebackend/pojo/response/UserResponse.java
+++ src/main/java/ee/taltech/twentyonebackend/pojo/response/UserResponse.java
@@ -1,5 +1,10 @@
 package ee.taltech.twentyonebackend.pojo.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserResponse {
 	private String username;
 	private String token;
